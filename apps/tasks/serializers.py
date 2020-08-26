8714f35ad077c382d4b8fae9277d7dd62fea9283
Hovhanes <27626hovo@gmail.com>
2020-08-26 16:16:14 +0400

    add username in serializer

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -4,7 +4,18 @@ from apps.tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+    )
+
+    created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    updated_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+
     class Meta:
         model = Task
-
         fields = ['id', 'title', 'description', 'is_done', 'user', 'created_at', 'updated_at']
+
+    def to_representation(self, instance):
+        data = super(TaskSerializer, self).to_representation(instance)
+        data.update({"user": instance.user.username})
+        return data
